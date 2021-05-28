5d9fa47ff7c67e085f51eb96d30ef1de89391e3c
Krishna Sah <krishnasahk8356@gmail.com>
2021-05-28 17:22:46 +0530

    Handled cart and checkout page for logged in users

--- shopcart/settings.py
+++ shopcart/settings.py
@@ -144,6 +144,6 @@ MESSAGE_TAGS = {
 # SMTP configuration
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'Krishnasah984503432@gmail.com'
+EMAIL_HOST_USER = 'sahkrishna984503432@gmail.com'
 EMAIL_HOST_PASSWORD = 'Krishna@778'
 EMAIL_USE_TLS = True
